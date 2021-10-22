0b8326412100523c4a6d8640d42a8800e86b563e
Cory Craig <coryc257@gmail.com>
2021-10-22 18:07:57 -0400

    C code generation from template

--- generate_init_params.py
+++ generate_init_params.py
@@ -35,6 +35,29 @@ if __name__ == '__main__':
 	os.system("./generate_init_foil.py ./init.param > init.FN")
 	os.system("./generate_init_call.py ./init.param > init.SP")
 	os.system("./generate_init_setup.py ./init.param > init.ST")
+	
+	template = open("./slowboot.ct","r")
+	t_FN = open("./init.FN","r")
+	t_SP = open("./init.SP","r")
+	t_ST = open("./init.ST","r")
+	fillin = open("./slowboot.c","w")
+	t_FN_s = t_FN.read()
+	t_FN.close()
+	t_SP_s = t_SP.read()
+	t_SP.close()
+	t_ST_s = t_ST.read()
+	t_ST.close()
+	tstr = template.read()
+	template.close()
+
+	tstr = tstr.replace("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$FN",t_FN_s)
+	tstr = tstr.replace("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$SP",t_SP_s)
+	tstr = tstr.replace("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ST",t_ST_s)
+	
+	print(tstr)
+	
+	
+	
 	exit(0)
 	
 	with open(sys.argv[1]) as f:
