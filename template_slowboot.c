a8125975b230e08adb7be7e3bb810be9163cca10
Cory Craig <coryc257@gmail.com>
2021-11-06 20:21:21 -0400

    Added in code for everything but the sig verification of the file Compiles, untested

--- template_slowboot.c
+++ template_slowboot.c
@@ -6,6 +6,8 @@
 #include <crypto/aead.h>
 #include <crypto/hash.h>
 #include <crypto/skcipher.h>
+#include <crypto/akcipher.h>
+#include <crypto/public_key.h>
 #include <linux/err.h>
 #include <linux/fips.h>
 #include <linux/gfp.h>
@@ -30,13 +32,13 @@
 DEFINE_MUTEX(gs_concurrency_locker);
 
 #define SHA512_HASH_LEN 130
-
+#define NEW_LINE '\n'
 #define GLOW printk(KERN_ERR "GLOWING\n");
 
 typedef struct slowboot_validation_item {
 	char hash[130];
 	u8 b_hash[65];
-	char path[PATH_MAX];
+	char path[PATH_MAX+1];
 	int is_ok;
 	char *buf;
 	size_t buf_len;
@@ -74,6 +76,121 @@ static int __get_slwbt_ct(void)
 {
 	return tinfoil.slwbt_ct;
 }
+
+//openssl x509 -C -in my_signing_key_pub.der -inform DER
+//xxd -i siggywiggy.sha512
+//xxd -ps -c 9999999 siggywiggy.sha512 > siggywiggy.hex
+
+
+// It helps if you are using an RSA key
+//openssl genrsa -aes256 -passout pass:<phrase> -out private.pem 4096
+//openssl rsa -in private.pem -passin pass:<phrase> -pubout -out public.pem
+//openssl dgst -sha256 -sign <private-key> -out /tmp/sign.sha256 <file>
+//openssl base64 -in /tmp/sign.sha256 -out <signature>
+//openssl base64 -d -in <signature> -out /tmp/sign.sha256
+//openssl dgst -sha256 -verify <pub-key> -signature /tmp/sign.sha256 <file>
+//openssl asn1parse -inform PEM -in public.pem -strparse 19 -out kernel.key
+//xxd -i kernel.key
+
+//openssl rsa -in private.pem -passin pass:1111 -pubout -out public.pem
+
+
+int local_public_key_verify_signature(const struct public_key *pkey,
+                const struct public_key_signature *sig)
+{
+    struct crypto_wait cwait;
+    struct crypto_akcipher *tfm;
+    struct akcipher_request *req;
+    struct scatterlist src_tab[3];
+    const char *alg_name;
+    char alg_name_buf[CRYPTO_MAX_ALG_NAME];
+    void *output;
+    unsigned int outlen;
+    int ret;
+
+    pr_devel("==>%s()\n", __func__);
+
+    BUG_ON(!pkey);
+    BUG_ON(!sig);
+    BUG_ON(!sig->s);
+
+    if (!sig->digest)
+        return -ENOPKG;
+
+    alg_name = sig->pkey_algo;
+    if (strcmp(sig->pkey_algo, "rsa") == 0) {
+        /* The data wangled by the RSA algorithm is typically padded
+         * and encoded in some manner, such as EMSA-PKCS1-1_5 [RFC3447
+         * sec 8.2].
+         */
+        if (snprintf(alg_name_buf, CRYPTO_MAX_ALG_NAME,
+                 "pkcs1pad(rsa,%s)", sig->hash_algo
+                 ) >= CRYPTO_MAX_ALG_NAME)
+            return -EINVAL;
+        alg_name = alg_name_buf;
+    }
+
+    printk(KERN_INFO "ALGO:%s\n", alg_name);
+    tfm = crypto_alloc_akcipher(alg_name, 0, 0);
+    if (IS_ERR(tfm)) {
+    	printk(KERN_ERR "tfm\n");
+        return PTR_ERR(tfm);
+    }
+
+    ret = -ENOMEM;
+    req = akcipher_request_alloc(tfm, GFP_KERNEL);
+    if (!req) {
+    	printk(KERN_ERR "2\n");
+        goto error_free_tfm;
+    }
+
+    ret = crypto_akcipher_set_pub_key(tfm, pkey->key, pkey->keylen);
+    if (ret) {
+    	printk(KERN_ERR "3\n");
+        goto error_free_req;
+    }
+
+    ret = -ENOMEM;
+    outlen = crypto_akcipher_maxsize(tfm);
+    output = kmalloc(outlen, GFP_KERNEL);
+    if (!output) {
+    	printk(KERN_ERR "4\n");
+        goto error_free_req;
+    }
+
+
+    sg_init_table(src_tab, 3);
+
+    sg_set_buf(&src_tab[1], sig->digest, sig->digest_size);
+    sg_set_buf(&src_tab[0], sig->s, sig->s_size);
+
+    akcipher_request_set_crypt(req, src_tab, NULL, sig->s_size, sig->digest_size);
+
+    crypto_init_wait(&cwait);
+    akcipher_request_set_callback(req, CRYPTO_TFM_REQ_MAY_BACKLOG |
+                      CRYPTO_TFM_REQ_MAY_SLEEP,
+                      crypto_req_done, &cwait);
+
+
+    ret = crypto_wait_req(crypto_akcipher_verify(req), &cwait);
+    if (ret) {
+    	printk(KERN_ERR "5\n");
+        goto out_free_output;
+    }
+    pr_info("verified successfuly!!!\n");
+
+out_free_output:
+    kfree(output);
+error_free_req:
+    akcipher_request_free(req);
+error_free_tfm:
+    crypto_free_akcipher(tfm);
+    pr_devel("<==%s() = %d\n", __func__, ret);
+    if (WARN_ON_ONCE(ret > 0))
+        ret = -EINVAL;
+    return ret;
+}
+
 
 /*******************************************************************************
 * Register data in array                                                       *
@@ -236,13 +353,97 @@ static int tinfoil_unwrap (slowboot_validation_item *item)
 }
 
 
+static loff_t fill_in_item(slowboot_validation_item *item, char *line, loff_t *remaining)
+{
+	loff_t pos, off, rem, pst;
+
+	pos = 0;
+	off = 0;
+	rem = *remaining;
+	pst = 0;
+
+
+	while(rem > 0) {
+
+		//printk(KERN_INFO "X:%d,%c\n",line[pos],line[pos]);
+
+		if (line[pos] == ' ' && off == 0 && rem > 2) {
+			off = pos+2;
+		}
+
+		if (line[pos] == NEW_LINE) {
+			pst = 1;
+			//line[pos] = '\0';
+			//printk(KERN_INFO "Line:%s\n", line);
+			//line[pos] = NEW_LINE;
+			break;
+		}
+
+		pos++;
+		rem--;
+	}
+
+	memset(item->path,0,PATH_MAX+1);
+	memset(item->hash,0,130);
+
+	// Make sure we have a good item
+	// This should not happen because who
+	// would sign something malicous?
+	if (pos > 133 && (pos-off-1) > 0) {
+		memcpy(item->hash, line, 128);
+		memcpy(item->path, line+off, pos-off);
+	}
+
+	if (rem > 0) {
+		pos++;
+		rem--;
+	}
+	*remaining = rem;
+	return pos;
+}
+
 static void slowboot_init(void)
 {
-	struct file *fp;
-	size_t file_size;
-	loff_t pos;
-	int num_read;
-	char *buf;
+	struct file *fp, *sfp;
+	struct crypto_shash *halg;
+	sdesc *hsd;
+	size_t file_size, sfp_file_size;
+	loff_t pos, remaining, sfp_pos;
+	int num_read, sfp_num_read;
+	long int num_items;
+	char *buf, sfp_buf;
+	unsigned char *kernel_key;
+	unsigned char *digest;
+	slowboot_validation_item *items, *c_item;
+	int kernel_key_len;
+
+	kernel_key_len = sizeof(tinfoil.config_pkey)/2;
+	kernel_key = kmalloc(kernel_key_len+1, GFP_KERNEL);
+	if (hex2bin(kernel_key, tinfoil.config_pkey, kernel_key_len) == 0) {
+		kernel_key[kernel_key_len] = '\0';
+		printk(KERN_INFO "KERNEL KEY:%s\n", kernel_key);
+	}
+
+
+	struct public_key rsa_pub_key = {
+		.key = kernel_key,
+		.keylen = kernel_key_len,
+		.pkey_algo = "rsa",
+		.id_type = "X509"
+
+	};
+
+
+
+	/*struct public_key_signature sig = {
+		.s = siggywiggy_sha512,
+		.s_size = siggywiggy_sha512_len,
+		.digest = b_hash,
+		.digest_size = 64,
+		.pkey_algo = "rsa",
+		.hash_algo = "sha512"
+	};*/
+
 
 	printk(KERN_INFO "Beginning SlowBoot 3 '%s'\n", tinfoil.config_file);
 
@@ -251,20 +452,76 @@ static void slowboot_init(void)
 	file_size = fp->f_pos;
 	default_llseek(fp, fp->f_pos * -1, SEEK_CUR);
 
+	sfp = filp_open(tinfoil.config_file_signature, O_RDONLY, 0);
+	default_llseek(sfp, 0, SEEK_END);
+	sfp_file_size = sfp->f_pos;
+	default_llseek(sfp, sfp->f_pos * -1, SEEK_CUR);
+
 	buf = vmalloc(file_size+1);
+	sfp_buf = vmalloc(sfp_file_size+1);
 	pos = 0;
+	sfp_pos = 0;
 
 	printk(KERN_INFO "Beginning SlowBoot 3 '%s'::'%d'\n", tinfoil.config_file, file_size);
 
 	num_read = kernel_read(fp,buf,file_size,&pos);
+	sfp_num_read = kernel_read(sfp,sfp_buf,sfp_file_size,&sfp_pos);
 
 	if (num_read != file_size) {
 		printk(KERN_ERR "File Read Error, size mismatch:%d:%d\n", num_read, file_size);
 	}
 
+	if (sfp_num_read != sfp_file_size) {
+		printk(KERN_ERR "File Read Error, size mismatch:%d:%d\n", sfp_num_read, sfp_file_size);
+	}
+
+	halg = crypto_alloc_shash("sha512",0,0);
+	digest = kmalloc(64, GFP_KERNEL);
+	memset(digest,0,64);
+	hsd = init_sdesc(halg);
+
+	crypto_shash_digest(&(hsd->shash), buf, file_size, digest);
+
+	struct public_key_signature sig = {
+		.s = sfp_buf,
+		.s_size = sfp_file_size,
+		.digest = digest,
+		.digest_size = 64,
+		.pkey_algo = "rsa",
+		.hash_algo = "sha512"
+	};
+
+	num_items = 0;
+
+	for (pos = 0; pos < file_size; pos++){
+		if (buf[pos] == '\n') {
+			num_items++;
+		}
+	}
+
+	if (num_items == 0)
+		goto out;
+
+	c_item = items = vmalloc(sizeof(slowboot_validation_item)*num_items);
+
+	if (!c_item) {
+		printk(KERN_ERR "Bad juju\n");
+	}
+
+	pos = 0;
+	remaining = file_size;
+	while (remaining){
+		//printk(KERN_INFO "fii:%d\n", pos);
+		pos += fill_in_item(c_item, &buf[pos], &remaining);
+		c_item++;
+	}
+
+
+	out:
 	filp_close(fp, NULL);
 	printk(KERN_INFO "Beginning SlowBoot4:%d\n", num_read);
-	tinfoil.slwbt_ct = 0;
+	tinfoil.validation_items = items;
+	tinfoil.slwbt_ct = num_items;
 }
 
 /*******************************************************************************
@@ -284,6 +541,7 @@ static void slowboot_run_test(void)
 	mutex_unlock(&gs_concurrency_locker);
 
 	for (j = 0; j < SLWBT_CT; j++) {
+		printk(KERN_INFO "SBI:%d\n",j);
 		tinfoil.failures += tinfoil_unwrap(
 			&(tinfoil.validation_items[j]));
 	}
