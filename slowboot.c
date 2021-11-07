c5c3d3697a026e8405890fb5fb9599d5943f5de9
Cory Craig <coryc257@gmail.com>
2021-11-07 11:54:39 -0500

    WOrking on code cleanup, periodic checkin

--- slowboot.c
+++ slowboot.c
@@ -29,6 +29,46 @@
 #define SLWBT_CT __get_slwbt_ct()
 #endif
 
+#ifndef SLWBT_CF
+#define SLWBT_CF "/testing/siggywiggy"
+#endif
+
+#ifndef SLWBT_CFS
+#define SLWBT_CFS "/testing/siggywiggy.sha512"
+#endif
+
+#ifndef SLWBT_PK
+#define SLWBT_PK "3082020a02820201009af1624ec932c82d57d296ebddf3d8c1cdc03a6c5c709cb3658b33797dd8a94b4183146224a63f8dbf04032690f04c4b05138cf9b0955057e4acf4721c84eb3073eeb1ccc5c6e9bec3d36b1b3bd274c13afb42f33c5c057121debaa622f8f2c0e75bbc99cbcf78767d4225025ece9561ca6022b650ab9c9a68763e7e461164bdfdd07b72e4c623e07b38a7767ac2671c06ea899d6291fddb1eb3d8a6d03fbd78719adec4b92f8881562d73923fcf8f2bd41f324993ecf42c40cd9c596c3b58850aa96a7d28a767b0be8e919fb247897cdc557391753db766991f197217b96e430c8e9bfc3f84a9c45b4aad9e6284e87041eb1709e99fd01e8f23f1f97aa86e255eb8d4bfb13ce6f14264347e40372bb79e17a87e1c541077e8e874092f475b9dbfb4fca981c1358971004421454069c3868cd4fe8fd1ea6d46d9daac7dc00d6b60d998bebbe0121126e3f29acfc3ccc2f24e6eb6c4ab9c0f2e7670e920f33d69eb1f0ca7be630098fe220c1f8ef87e51f8be663a70621a5932ee60888c7e40aa70313e1936bc0c6d742d2c2d2d46c2ceb2b3155ebc777f01bbfad07985e847f8d00c663706b92cf15fd2504ae8dd838d9576763e4ed12e2d6b0a5f7ea21bed613d371a96a25f2206fe6e1724cfcbf2c03d04dda6f623d0d31c036a63f030158478ae820020cf6eff88c70f335db426eeac8205a2875a393d5d67343d534ef54b0203010001"
+#endif
+
+#ifndef SLWBT_PKLEN
+#define SLWBT_PKLEN 1052
+#endif
+
+#ifndef SLWBT_DGLEN
+#define SLWBT_DGLEN 64
+#endif
+
+#ifndef SLWBT_HSLEN
+#define SLWBT_HSLEN 128
+#endif
+
+#ifndef SLWBT_PKALGO
+#define SLWBT_PKALGO "rsa"
+#endif
+
+#ifndef SLWBT_PKALGOPD
+#define SLWBT_PKALGOPD "pkcs1pad(rsa,%s)"
+#endif
+
+#ifndef SLWBT_HSALGO
+#define SLWBT_HSALGO "sha512"
+#endif
+
+#ifndef SLWBT_IDTYPE
+#define SLWBT_IDTYPE "X509"
+#endif
+
 DEFINE_MUTEX(gs_concurrency_locker);
 
 #define SHA512_HASH_LEN 130
@@ -36,8 +76,8 @@ DEFINE_MUTEX(gs_concurrency_locker);
 #define GLOW printk(KERN_ERR "GLOWING\n");
 
 typedef struct slowboot_validation_item {
-	char hash[130];
-	u8 b_hash[65];
+	char hash[SLWBT_HSLEN+2];
+	u8 b_hash[SLWBT_DGLEN+1];
 	char path[PATH_MAX+1];
 	int is_ok;
 	char *buf;
@@ -54,22 +94,22 @@ typedef struct slowboot_tinfoil {
 	int slwbt_ct;
 	char config_file[PATH_MAX];
 	char config_file_signature[PATH_MAX];
-	char config_pkey[1053];
+	char config_pkey[SLWBT_PKLEN+1];
 } slowboot_tinfoil;
 
 
-
-static u32 mode;
 static slowboot_tinfoil tinfoil = {
-		.config_file = "/testing/siggywiggy",
-		.config_file_signature = "/testing/siggywiggy.sha512",
-		.config_pkey = "3082020a02820201009af1624ec932c82d57d296ebddf3d8c1cdc03a6c5c709cb3658b33797dd8a94b4183146224a63f8dbf04032690f04c4b05138cf9b0955057e4acf4721c84eb3073eeb1ccc5c6e9bec3d36b1b3bd274c13afb42f33c5c057121debaa622f8f2c0e75bbc99cbcf78767d4225025ece9561ca6022b650ab9c9a68763e7e461164bdfdd07b72e4c623e07b38a7767ac2671c06ea899d6291fddb1eb3d8a6d03fbd78719adec4b92f8881562d73923fcf8f2bd41f324993ecf42c40cd9c596c3b58850aa96a7d28a767b0be8e919fb247897cdc557391753db766991f197217b96e430c8e9bfc3f84a9c45b4aad9e6284e87041eb1709e99fd01e8f23f1f97aa86e255eb8d4bfb13ce6f14264347e40372bb79e17a87e1c541077e8e874092f475b9dbfb4fca981c1358971004421454069c3868cd4fe8fd1ea6d46d9daac7dc00d6b60d998bebbe0121126e3f29acfc3ccc2f24e6eb6c4ab9c0f2e7670e920f33d69eb1f0ca7be630098fe220c1f8ef87e51f8be663a70621a5932ee60888c7e40aa70313e1936bc0c6d742d2c2d2d46c2ceb2b3155ebc777f01bbfad07985e847f8d00c663706b92cf15fd2504ae8dd838d9576763e4ed12e2d6b0a5f7ea21bed613d371a96a25f2206fe6e1724cfcbf2c03d04dda6f623d0d31c036a63f030158478ae820020cf6eff88c70f335db426eeac8205a2875a393d5d67343d534ef54b0203010001",
+		.config_file = SLWBT_CF,
+		.config_file_signature = SLWBT_CFS,
+		.config_pkey = SLWBT_PK,
 		.initialized = 1
 };
+/*
 static slowboot_validation_item tinfoil_items[] = {
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$DT
 		//{.hash="", .path=""},
-};
+};*/
+
 //static struct kstat *st;
 
 static int __get_slwbt_ct(void)
@@ -118,13 +158,13 @@ int local_public_key_verify_signature(const struct public_key *pkey,
         return -ENOPKG;
 
     alg_name = sig->pkey_algo;
-    if (strcmp(sig->pkey_algo, "rsa") == 0) {
+    if (strcmp(sig->pkey_algo, SLWBT_PKALGO) == 0) {
         /* The data wangled by the RSA algorithm is typically padded
          * and encoded in some manner, such as EMSA-PKCS1-1_5 [RFC3447
          * sec 8.2].
          */
         if (snprintf(alg_name_buf, CRYPTO_MAX_ALG_NAME,
-                 "pkcs1pad(rsa,%s)", sig->hash_algo
+        		     SLWBT_PKALGOPD, sig->hash_algo
                  ) >= CRYPTO_MAX_ALG_NAME)
             return -EINVAL;
         alg_name = alg_name_buf;
@@ -190,19 +230,6 @@ error_free_tfm:
         ret = -EINVAL;
     return ret;
 }
-
-
-/*******************************************************************************
-* Register data in array                                                       *
-*******************************************************************************/
-/*static void svi_reg(slowboot_validation_item *item,
-		const char *hash,
-		const char *path)
-{
-	strncpy(item->hash, hash, SHA512_HASH_LEN);
-	strncpy(item->path, path, PATH_MAX);
-}*/
-
 
 static int tinfoil_open(slowboot_validation_item *item)
 {
@@ -297,13 +324,13 @@ static void tinfoil_check(slowboot_validation_item *item)
 	unsigned char *digest;
 	int j;
 	
-	alg = crypto_alloc_shash("sha512", 0, 0);
+	alg = crypto_alloc_shash(SLWBT_HSALGO, 0, 0);
 	if (IS_ERR(alg)) {
 		printk(KERN_ERR "Can't allocate alg\n");
 	}
 	
-	digest = kmalloc(64, GFP_KERNEL);
-	memset(digest,0,64);
+	digest = kmalloc(SLWBT_DGLEN+1, GFP_KERNEL);
+	memset(digest,0,SLWBT_DGLEN+1);
 	sd = init_sdesc(alg);
 	if (!sd) {
 	  printk(KERN_ERR "Can't allocate alg\n");
@@ -315,7 +342,7 @@ static void tinfoil_check(slowboot_validation_item *item)
 	crypto_shash_digest(&(sd->shash), item->buf, item->buf_len, digest);
 	vfree(item->buf);
 	kfree(sd);
-	for(j=0;j<64;j++){
+	for(j=0;j<SLWBT_DGLEN;j++){
 		if(item->b_hash[j]!=digest[j]) {
 			item->is_ok = 1;
 		}
@@ -384,13 +411,13 @@ static loff_t fill_in_item(slowboot_validation_item *item, char *line, loff_t *r
 	}
 
 	memset(item->path,0,PATH_MAX+1);
-	memset(item->hash,0,130);
+	memset(item->hash,0,SLWBT_HSLEN+2);
 
 	// Make sure we have a good item
 	// This should not happen because who
 	// would sign something malicous?
-	if (pos > 133 && (pos-off-1) > 0) {
-		memcpy(item->hash, line, 128);
+	if (pos > (SLWBT_HSLEN+5) && (pos-off-1) > 0) {
+		memcpy(item->hash, line, SLWBT_HSLEN);
 		memcpy(item->path, line+off, pos-off);
 	}
 
@@ -402,36 +429,73 @@ static loff_t fill_in_item(slowboot_validation_item *item, char *line, loff_t *r
 	return pos;
 }
 
-static void slowboot_init(void)
+static int slowboot_init(void)
 {
 	struct file *fp, *sfp;
 	struct crypto_shash *halg;
 	sdesc *hsd;
 	size_t file_size, sfp_file_size;
 	loff_t pos, remaining, sfp_pos;
-	int num_read, sfp_num_read;
+	int num_read, sfp_num_read, status;
 	long int num_items;
 	char *buf, *sfp_buf;
 	unsigned char *kernel_key;
 	unsigned char *digest;
 	slowboot_validation_item *items, *c_item;
 	int kernel_key_len;
+	//struct public_key_signature sig;
+	struct public_key rsa_pub_key;
 
-	kernel_key_len = sizeof(tinfoil.config_pkey)/2;
+	// Trust nothing
+	status = 0;
+	fp = NULL; //filp_close
+	sfp = NULL; //filp_close
+	halg = NULL; //kfree
+	file_size = 0;
+	sfp_file_size = 0;
+	pos = 0;
+	remaining = 0;
+	sfp_pos = 0;
+	num_read = 0;
+	sfp_num_read = 0;
+	status = 0;
+	num_items = 0;
+	buf = NULL; //vfree
+	sfp_buf = NULL; //vfree
+	kernel_key = NULL; //kfree
+	digest = NULL; //kfree
+	items = NULL; //vfree
+	c_item = NULL; //set to null
+	kernel_key_len = 0;
+	rsa_pub_key.pkey_algo = SLWBT_PKALGO;
+	rsa_pub_key.id_type = SLWBT_IDTYPE;
+
+
+	kernel_key_len = SLWBT_PKLEN/2;
 	kernel_key = kmalloc(kernel_key_len+1, GFP_KERNEL);
+
+	if(!kernel_key) {
+		goto fail;
+	}
+
 	if (hex2bin(kernel_key, tinfoil.config_pkey, kernel_key_len) == 0) {
 		kernel_key[kernel_key_len] = '\0';
 		printk(KERN_INFO "KERNEL KEY:%s\n", kernel_key);
+	} else {
+		goto fail;
 	}
 
 
+	rsa_pub_key.key = kernel_key;
+	rsa_pub_key.keylen = kernel_key_len;
+	/*
 	struct public_key rsa_pub_key = {
 		.key = kernel_key,
 		.keylen = kernel_key_len,
-		.pkey_algo = "rsa",
-		.id_type = "X509"
+		.pkey_algo = SLWBT_PKALGO,
+		.id_type = SLWBT_IDTYPE
 
-	};
+	};*/
 
 
 
@@ -449,7 +513,7 @@ static void slowboot_init(void)
 
 	if (IS_ERR(fp = filp_open(tinfoil.config_file, O_RDONLY, 0))) {
 		printk(KERN_ERR "flip open fp\n");
-		return;
+		return status;
 	}
 	default_llseek(fp, 0, SEEK_END);
 	file_size = fp->f_pos;
@@ -457,7 +521,7 @@ static void slowboot_init(void)
 
 	if (IS_ERR(sfp = filp_open(tinfoil.config_file_signature, O_RDONLY, 0))) {
 		printk(KERN_ERR "flip open sfp\n");
-		return;
+		return status;
 	}
 	default_llseek(sfp, 0, SEEK_END);
 	sfp_file_size = sfp->f_pos;
@@ -465,31 +529,37 @@ static void slowboot_init(void)
 
 	if((buf = vmalloc(file_size+1)) == NULL) {
 		printk(KERN_ERR "alloc buf\n");
-		return;
+		return status;
 	}
 	if ((sfp_buf = vmalloc(sfp_file_size+1)) == NULL) {
 		printk(KERN_ERR "alloc sfp_buf\n");
-		return;
+		return status;
 	}
 	pos = 0;
 	sfp_pos = 0;
 
-	printk(KERN_INFO "Beginning SlowBoot 3 '%s'::'%d'\n", tinfoil.config_file, file_size);
+	printk(KERN_INFO "Beginning SlowBoot 3 '%s'::'%ld'\n",
+		   tinfoil.config_file,
+		   file_size);
 
 	num_read = kernel_read(fp,buf,file_size,&pos);
 	sfp_num_read = kernel_read(sfp,sfp_buf,sfp_file_size,&sfp_pos);
 
 	if (num_read != file_size) {
-		printk(KERN_ERR "File Read Error, size mismatch:%d:%d\n", num_read, file_size);
+		printk(KERN_ERR "File Read Error, size mismatch:%d:%ld\n",
+			   num_read,
+			   file_size);
 	}
 
 	if (sfp_num_read != sfp_file_size) {
-		printk(KERN_ERR "File Read Error, size mismatch:%d:%d\n", sfp_num_read, sfp_file_size);
+		printk(KERN_ERR "File Read Error, size mismatch:%d:%ld\n",
+			   sfp_num_read,
+			   sfp_file_size);
 	}
 
-	halg = crypto_alloc_shash("sha512",0,0);
-	digest = kmalloc(64, GFP_KERNEL);
-	memset(digest,0,64);
+	halg = crypto_alloc_shash(SLWBT_HSALGO,0,0);
+	digest = kmalloc(SLWBT_DGLEN+1, GFP_KERNEL);
+	memset(digest,0,SLWBT_DGLEN+1);
 	hsd = init_sdesc(halg);
 
 	crypto_shash_digest(&(hsd->shash), buf, file_size, digest);
@@ -498,9 +568,9 @@ static void slowboot_init(void)
 		.s = sfp_buf,
 		.s_size = sfp_file_size,
 		.digest = digest,
-		.digest_size = 64,
-		.pkey_algo = "rsa",
-		.hash_algo = "sha512"
+		.digest_size = SLWBT_DGLEN,
+		.pkey_algo = SLWBT_PKALGO,
+		.hash_algo = SLWBT_HSALGO
 	};
 
 	if (local_public_key_verify_signature(&rsa_pub_key, &sig) == 0) {
@@ -534,12 +604,21 @@ static void slowboot_init(void)
 		c_item++;
 	}
 
-
-	out:
-	filp_close(fp, NULL);
-	printk(KERN_INFO "Beginning SlowBoot4:%d\n", num_read);
 	tinfoil.validation_items = items;
 	tinfoil.slwbt_ct = num_items;
+	goto out;
+
+fail:
+	tinfoil.slwbt_ct = 0;
+	if (!items) {
+		vfree(items);
+	}
+	status = 1;
+out:
+	filp_close(fp, NULL);
+	printk(KERN_INFO "Beginning SlowBoot4:%d\n", num_read);
+
+	return status;
 }
 
 /*******************************************************************************
